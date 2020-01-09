946cc7d2f03f5fa7d1183101bc04504adf4efba7
adam <adam@harpur.io>
2020-01-09 20:23:59 +0000

    chore: remove loading image

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import L from 'leaflet';
 import planes from '@utils/process';
 
+const NORTHERN_IRELAND = [54.607868, -7.0];
 const mapHandle = document.getElementById('map');
 const loading = document.createElement('img');
 loading.setAttribute('src', 'public/assets/loading.gif');
@@ -8,7 +9,7 @@ loading.setAttribute('height', '600');
 loading.setAttribute('width', '800');
 mapHandle.appendChild(loading);
 planes.then(markers => {
-    const NORTHERN_IRELAND = [54.607868, -7.0];
+    mapHandle.removeChild(loading);
 
     const map = L.map('map').setView(NORTHERN_IRELAND, 8);
     const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
